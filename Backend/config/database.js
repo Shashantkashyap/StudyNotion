88561adfc6b9fd2b2c9d622d2f8b13c1c383d645
Shashant <shashant.kashyap999@gmail.com>
2024-01-18 23:17:02 +0530

    x

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 exports.connect = () => {
   mongoose
-    .connect(process.env.MONGODB_URL)
+    .connect("mongodb+srv://Ed-tech:Shashant123@ed-tech.2x94kvd.mongodb.net/Ed-tech?retryWrites=true&w=majority")
     .then(() => console.log("DB connected successfully"))
     .catch((error) => {
       console.log("DB connection failed");
